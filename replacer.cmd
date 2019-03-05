f7d9877e74373a774f60d449644531f15b26d745
Zbigniew N <zbign@o2.pl>
2019-03-05 20:00:29 +0100

    Revert "initial commit"
    This reverts commit f2262bfd456e940b26851107ac756a3e7c57effb.

--- replacer.cmd
+++
@@ -1,6 +0,0 @@
-rem powershell -Command "(gc fce.xml) -replace '<item>', '<entry>' | Out-File blogger.xml"
-
-powershell -ExecutionPolicy ByPass -File replacer.ps1
-
-pause
-rem  -replace '(?<=<center>).*?(?=</center>)', 'This is my new text.'
